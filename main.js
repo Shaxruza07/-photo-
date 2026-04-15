e0313cd42665bb5738e9d208e347edd70fefc350
Shaxruza07 <rejabovashakhruza@gmail.com>
2026-04-15 17:32:48 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const btnLeft = document.getElementById("btnLeft");
+const btnRight = document.getElementById("btnRight");
+const slide = document.getElementById("slide");
+const showNumber = document.getElementById("showNumber");
+
+let imgs = ["./images/image-one.jpg","./images/image-two.jpg","./images/image-three.jpg", "./images/image-four.avif","./images/image-five.jpg"]
+
+let active = 0;
+
+showNumber.innerHTML = active +1
+
+btnRight.addEventListener("click", () => {
+  active++;
+
+  if (active >= imgs.length) {
+    active = 0;
+  }
+
+  slide.src = imgs[active];
+  showNumber.innerHTML = active +1
+});
+
+btnLeft.addEventListener("click", () => {
+  active--;
+
+  if (active < 0) {
+    active = imgs.length - 1;
+  }
+
+  slide.src = imgs[active];
+  showNumber.innerHTML = active +1
+});
+
+
